d64556fef1d89a0871430082f3a7d251e7bdec26
Eric <ekfuhrmann@gmail.com>
2023-11-06 17:04:33 -0700

    Initial GrubMarket Animation

---
+++ src/scripts/app.ts
@@ -0,0 +1,131 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+interface AnimatedElement extends Element {
+  dataset?: DOMStringMap
+  timeline?: gsap.core.Timeline
+}
+
+// only run when there is no preference for reduced motion
+gsap.matchMedia().add('(prefers-reduced-motion: no-preference)', () => {
+  batchAnimation(gsap.utils.toArray('[data-animate]'))
+  batchAnimation(gsap.utils.toArray('[data-animate-text]'))
+  batchAnimation(gsap.utils.toArray('[data-animate-assets]'))
+})
+
+function batchAnimation(elements: AnimatedElement[] | string) {
+  if (!elements.length) return
+
+  gsap.set(elements, { opacity: 0 })
+
+  ScrollTrigger.batch(elements, {
+    batchMax: 5,
+    onEnter: (batch) => {
+      batch.forEach((el: AnimatedElement, i: number) => {
+        setAnimation(el, i)
+      })
+    },
+    onEnterBack: (batch) => {
+      batch.forEach((el: AnimatedElement, i: number) => {
+        setAnimation(el, i, -1)
+      })
+    },
+    onLeave: (batch) => {
+      batch.forEach((el: AnimatedElement) => {
+        // kill the timeline when leaving the viewport
+        el.timeline?.kill()
+        gsap.to(el, { opacity: 0 })
+      })
+    },
+    onLeaveBack: (batch) => {
+      batch.forEach((el: AnimatedElement) => {
+        // kill the timeline when leaving the viewport
+        el.timeline?.kill()
+        gsap.to(el, { opacity: 0 })
+      })
+    },
+  })
+}
+
+function setAnimation(
+  el: AnimatedElement,
+  index: number = 0,
+  direction?: -1 | 1,
+) {
+  direction = direction || 1
+  let x = 0,
+    y = 0
+
+  const timeline = gsap.timeline()
+  const animationStagger = 0.15
+
+  const animateDirection = getAnimationDirection(el)
+
+  switch (animateDirection) {
+    case 'up':
+      y = 75 * direction
+      break
+
+    case 'down':
+      y = -75
+      break
+
+    case 'left':
+      x = -75
+      break
+
+    case 'right':
+      x = 75
+      break
+
+    default:
+      break
+  }
+
+  const initialX = gsap.getProperty(el, 'x')
+  const initialY = gsap.getProperty(el, 'y')
+
+  timeline.fromTo(
+    el,
+    { opacity: 0, y, x, scale: 0.925 },
+    {
+      opacity: 1,
+      scale: 1,
+      y: initialY,
+      x: initialX,
+      delay: index * animationStagger,
+      ease: 'circ.out',
+      duration: 1,
+      clearProps: 'x,y',
+    },
+  )
+
+  // store the timeline on the element
+  el.timeline = timeline
+}
+
+function getAnimationDirection(el: AnimatedElement) {
+  // Iterate over the dataset object
+  for (const attr in el.dataset) {
+    // Check if the attribute starts with 'animate'
+    if (attr.startsWith('animate')) {
+      // Return the value of the attribute
+      return el.dataset[attr]
+    }
+  }
+}
+
+// Parallax effect
+gsap.to('[data-speed]', {
+  y: (i, el) =>
+    (1 - parseFloat(el.getAttribute('data-speed'))) *
+    ScrollTrigger.maxScroll(window),
+  ease: 'none',
+  scrollTrigger: {
+    start: 0,
+    scrub: 0.5,
+    invalidateOnRefresh: true,
+  },
+})

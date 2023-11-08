a37dad6652a0cc5360394de450579172c429af67
Eric <ekfuhrmann@gmail.com>
2023-11-08 11:14:52 -0700

    Streamlined animations

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -3,27 +3,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-interface AnimatedElement extends Element {
+export interface AnimatedElement extends Element {
   dataset?: DOMStringMap
   timeline?: gsap.core.Timeline
+}
+
+export const mmFilters = {
+  isDesktop: '(min-width: 768px)',
+  isMobile: '(max-width: 767.98px)',
+  reduceMotion: '(prefers-reduced-motion: reduce)',
 }
 
 const mm = gsap.matchMedia()
 
 // only run when there is no preference for reduced motion
-mm.add('(prefers-reduced-motion: no-preference)', () => {
-  batchAnimation(gsap.utils.toArray('[data-animate]'))
-  batchAnimation(gsap.utils.toArray('[data-animate-text]'))
-  batchAnimation(gsap.utils.toArray('[data-animate-assets]'))
-})
+mm.add(mmFilters, (context) => {
+  let { reduceMotion } = context.conditions ?? {}
 
-function batchAnimation(elements: AnimatedElement[] | string) {
-  if (!elements.length) return
+  gsap.set('[data-animate]', { opacity: 0 })
 
-  gsap.set(elements, { opacity: 0 })
-
-  ScrollTrigger.batch(elements, {
-    batchMax: 5,
+  ScrollTrigger.batch('[data-animate]', {
     onEnter: (batch) => {
       batch.forEach((el: AnimatedElement, i: number) => {
         setAnimation(el, i)
@@ -49,110 +48,90 @@ function batchAnimation(elements: AnimatedElement[] | string) {
       })
     },
   })
-}
 
-function setAnimation(
-  el: AnimatedElement,
-  index: number = 0,
-  direction?: -1 | 1,
-) {
-  direction = direction || 1
-  let x = 0,
-    y = 0
+  function setAnimation(
+    el: AnimatedElement,
+    index: number = 0,
+    direction?: -1 | 1,
+  ) {
+    direction = direction || 1
+    let x = 0,
+      y = 0
 
-  const timeline = gsap.timeline()
-  const animationStagger = 0.15
+    const timeline = gsap.timeline()
+    const animationStagger = 0.15
 
-  const animateDirection = getAnimationDirection(el)
+    switch (el.dataset?.animate) {
+      case 'up':
+        y = reduceMotion ? 0 : 75 * direction
+        break
 
-  switch (animateDirection) {
-    case 'up':
-      y = 75 * direction
-      break
+      case 'down':
+        y = reduceMotion ? 0 : -75
+        break
 
-    case 'down':
-      y = -75
-      break
+      case 'left':
+        x = reduceMotion ? 0 : -75
+        break
 
-    case 'left':
-      x = -75
-      break
+      case 'right':
+        x = reduceMotion ? 0 : 75
+        break
 
-    case 'right':
-      x = 75
-      break
-
-    default:
-      break
-  }
-
-  const initialX = gsap.getProperty(el, 'x')
-  const initialY = gsap.getProperty(el, 'y')
-
-  timeline.fromTo(
-    el,
-    { opacity: 0, y, x, scale: 0.925 },
-    {
-      opacity: 1,
-      scale: 1,
-      y: initialY,
-      x: initialX,
-      delay: index * animationStagger,
-      ease: 'circ.out',
-      duration: 1,
-      clearProps: 'x,y',
-    },
-  )
-
-  // store the timeline on the element
-  el.timeline = timeline
-}
-
-function getAnimationDirection(el: AnimatedElement) {
-  // Iterate over the dataset object
-  for (const attr in el.dataset) {
-    // Check if the attribute starts with 'animate'
-    if (attr.startsWith('animate')) {
-      // Return the value of the attribute
-      return el.dataset[attr]
+      default:
+        break
     }
+
+    const initialX = gsap.getProperty(el, 'x')
+    const initialY = gsap.getProperty(el, 'y')
+
+    timeline.fromTo(
+      el,
+      { opacity: 0, y, x, scale: reduceMotion ? 1 : 0.925 },
+      {
+        opacity: 1,
+        scale: 1,
+        y: initialY,
+        x: initialX,
+        delay: reduceMotion ? 0 : index * animationStagger,
+        ease: 'circ.out',
+        duration: 1,
+        clearProps: 'x,y',
+      },
+    )
+
+    // store the timeline on the element
+    el.timeline = timeline
   }
-}
+})
 
 // Parallax effect
-mm.add(
-  {
-    isDesktop: '(min-width: 768px)',
-    isMobile: '(max-width: 767.98px)',
-    reduceMotion: '(prefers-reduced-motion: reduce)',
-  },
-  (context) => {
-    let { isMobile, reduceMotion } = context.conditions ?? {}
+mm.add(mmFilters, (context) => {
+  let { isMobile, reduceMotion } = context.conditions ?? {}
 
-    if (reduceMotion) return
+  if (reduceMotion) return
 
-    gsap.to('[data-speed]', {
-      y: (i, el) => {
-        const speed = parseFloat(el.getAttribute('data-speed'))
-        let adjustedSpeed
+  gsap.to('[data-speed]', {
+    y: (i, el) => {
+      const speed = parseFloat(el.getAttribute('data-speed'))
+      let adjustedSpeed
 
-        // adjust speed on mobile to be half that of desktop
-        if (speed < 1 && isMobile) {
-          adjustedSpeed = speed + (1 - speed) / 2
-        } else if (speed > 1 && isMobile) {
-          adjustedSpeed = speed - (speed - 1) / 2
-        } else {
-          adjustedSpeed = speed
-        }
+      // adjust speed on mobile to be half that of desktop
+      if (speed < 1 && isMobile) {
+        adjustedSpeed = speed + (1 - speed) / 2
+      } else if (speed > 1 && isMobile) {
+        adjustedSpeed = speed - (speed - 1) / 2
+      } else {
+        adjustedSpeed = speed
+      }
 
-        return (1 - adjustedSpeed) * ScrollTrigger.maxScroll(window)
-      },
-      ease: 'none',
-      scrollTrigger: {
-        start: 0,
-        scrub: 0.5,
-        invalidateOnRefresh: true,
-      },
-    })
-  },
-)
+      return (1 - adjustedSpeed) * ScrollTrigger.maxScroll(window)
+    },
+    ease: 'none',
+    scrollTrigger: {
+      start: 0,
+      scrub: 0.5,
+      invalidateOnRefresh: true,
+    },
+  })
+})
